657b474d4ae45b9fbf600ef8914b9bae13040b87
Holeil <holeilaccougm@gmail.com>
2018-09-12 20:28:59 +0300

    add smart func sort

--- src/index.js
+++ src/index.js
@@ -1,43 +1,44 @@
 class Sorter {
   constructor() {
     this.array = new Array();
-  }
+    this.compareFunction = function(a, b) {return a - b};
 
+  }
+  
   add(element) {
     this.array.push(element);
+
   }
 
   at(index) {
     return this.array[index];
+
   }
 
   get length() {
     return this.array.length;
+
   }
 
   toArray() {
     return this.array;
+
   }
 
   sort(indices) {
-    function compareNumeric(a, b) {
-      if (a > b) return 1;
-      if (a < b) return -1;
-    }
-    indices.sort(compareNumeric);
-    for(var x=0; x < indices.length; x++) {
-      for(var y=x+1; y < indices.length; y++) {
-        if(this.array[indices[x]] > this.array[indices[y]]) {
-          let a = this.array[indices[y]];
-          this.array[indices[y]] = this.array[indices[x]];
-          this.array[indices[x]] = a;
-        }
-      }
-    }
+    indices.sort((a, b) => a - b);
+    var newArr = [];
+    indices.forEach(item => {
+      newArr.push(this.array[item]);
+    });
+    newArr.sort(this.compareFunction);
+    indices.forEach((item, index) => {
+      this.array[item] = newArr[index]
+    });
   }
 
   setComparator(compareFunction) {
-
+    this.compareFunction = compareFunction;
   }
 }
 module.exports = Sorter;
